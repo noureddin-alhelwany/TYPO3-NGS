bb5565a535d0b82bef21613d2aa925ced5a7889b
Mhd Ghaith Alhelwany <m.g.alhelwany@gmail.com>
2025-03-28 21:05:21 +0300

    add tailwind css support

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import typo3 from "vite-plugin-typo3";
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-    plugins: [typo3()],
+    plugins: [typo3(), tailwindcss()],
 });
