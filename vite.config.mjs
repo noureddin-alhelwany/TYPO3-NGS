6b8b51578124f208e214e5fad315d1514fd0ba01
Mhd Ghaith Alhelwany <m.g.alhelwany@gmail.com>
2025-04-02 13:23:00 +0200

    fix tailwind dynamic classes issue

--- vite.config.mjs
+++ vite.config.mjs
@@ -4,7 +4,19 @@ import tailwindcss from '@tailwindcss/vite'
 import vuePlugin from "@vitejs/plugin-vue";
 
 export default defineConfig({
-    plugins: [typo3(), tailwindcss(), vuePlugin()],
+    plugins: [typo3(), tailwindcss({
+        config: {
+            content: [
+                "./packages/template/ContentBlocks/**/*.html",
+                "./packages/template/Resources/**/*.{html,css,vue}",
+            ],
+        },
+        safelist: [
+            'bg-primary', 'bg-on-primary', 'bg-secondary', 'bg-on-secondary',
+            'text-primary', 'text-on-primary', 'text-secondary', 'text-on-secondary',
+            'border-primary', 'border-on-primary', 'border-secondary', 'border-on-secondary',
+        ],
+    }), vuePlugin()],
     define: {
         __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: 'false',
         __VUE_PROD_DEVTOOLS__: 'false',
