e28d57e78c7f2851f8864987e904e23d48860968
noureddin <noureddin.alhelwany@gmail.com>
2025-04-16 13:14:45 +0200

    Remove alert and bind vue app

--- packages/template/Resources/Private/assets/askvwl/Main.entry.js
+++ packages/template/Resources/Private/assets/askvwl/Main.entry.js
@@ -1,1 +1,13 @@
-alert('askvwl');
+import { createApp } from 'vue/dist/vue.esm-bundler';
+
+import StyledButton from "../shared/Components/StyledButton.vue"
+createApp({
+    data() {
+        return {
+        }
+    },
+    components: {
+        StyledButton
+    }
+}).mount('#app')
+
